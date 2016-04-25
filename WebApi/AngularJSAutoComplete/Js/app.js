b2dd8166c237b07ac23894ad3d84f7b2f91d798f
Akhtar Uzzaman <akhtaruzzaman05@gmail.com>
2016-04-25 15:55:01 +0600

    Barcode added

---
+++ WebApi/AngularJSAutoComplete/Js/app.js
@@ -0,0 +1,25 @@
+﻿var app = angular.module('myApp', ['autocomplte-alt']);
+app.controller('ngAutoComplteController', ['$scope','$http', function($scope, $http) {
+    $scope.FirstNames = [];
+    $scope.SelectdFirstName = null;
+
+
+    //After Select Name
+    $scope.AfterSeletedFirstName=function(selected) {
+        if (selected) {
+            $scope.SelectdFirstName = selected.originalObject;
+        }
+    }
+
+    // Populate Data from database
+    $http({
+        method: 'Get',
+        url: 'api/information/AutoCompleteGet'
+    }).then(function(data) {
+        $scope.FirstNames = data.data;
+    }, function() {
+        alert('Error');
+    });
+}]);
+
+
